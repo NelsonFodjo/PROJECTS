24bb886f93ce91c1841cb2dbd96889099f3a0379
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-07 21:35:28 +0400

    Improved visual appearance and styling
    Changes include: Modified index.html, Modified script.js, Modified style.css
    
    Modified 34 lines (+26 additions, -8 deletions)

--- JAVASCRIPT/COUNTER/script.js
+++ JAVASCRIPT/COUNTER/script.js
@@ -1,4 +1,4 @@
-const add = document.querySelector(".numb p")
+const addDisplay = document.querySelector(".numb p")
 
 let count = 0;
 
@@ -11,10 +11,24 @@ function updateval(){
     document.getElementById("count").innerHTML = count;
 }
 
-function substract(){
+function subtract(){
     count --;
+    updateval()
+}
+
+function reset(){
+    count = 0;
+    updateval();
 }
 
 function save(){
-    
+    localStorage.setItem("count", count);
+}
+
+function load(){
+    let saved = localStorage.getItem("count")
+    if (saved != null){
+        count = Number(saved)
+    }
+    updateval()
 }
