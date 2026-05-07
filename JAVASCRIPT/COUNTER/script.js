46c43d85daa8cacd722d6feff970b6b199b7cb04
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-07 21:05:29 +0400

    Enhanced 2 files with improvements
    Changes include: Modified index.html, Modified script.js
    
    Modified 21 lines (+20 additions, -1 deletions)

--- JAVASCRIPT/COUNTER/script.js
+++ JAVASCRIPT/COUNTER/script.js
@@ -1,2 +1,20 @@
 const add = document.querySelector(".numb p")
 
+let count = 0;
+
+function add(){
+    count++;
+    updateval();
+}
+
+function updateval(){
+    document.getElementById("count").innerHTML = count;
+}
+
+function substract(){
+    count --;
+}
+
+function save(){
+    
+}
