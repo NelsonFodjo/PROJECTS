bbf08e7af04c51d90016fe3c61dc8156aa717fd5
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-06-10 16:56:34 +0400

    Enhanced test coverage and testing capabilities
    Changes include: Modified App.jsx
    
    Modified 5 lines (+4 additions, -1 deletions)

--- REACT/testing/src/App.jsx
+++ REACT/testing/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
-
+function Heading(){
+  return  <h1>This is an h1 heading</h1>
+}
 
 function App() {
 
@@ -7,6 +9,7 @@ function App() {
     <div>
       <h1> Nice Nelson React to the World</h1>
       <p>Time to Build Somthing Awesome!</p>
+      <Heading />
     </div>
     
   );
