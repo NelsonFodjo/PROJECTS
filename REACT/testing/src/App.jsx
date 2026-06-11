8ca55ce19cc5fb8f553491c3a70be1ac7729801c
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-06-11 15:26:01 +0400

    Enhanced test coverage and testing capabilities
    Changes include: Modified eslint.config.js, Modified package-lock.json, Modified App.css
    
    Modified 2674 lines (+9 additions, -2665 deletions)

--- REACT/testing/src/App.jsx
+++ REACT/testing/src/App.jsx
@@ -1,15 +1,18 @@
-import './App.css'
-function Heading(){
-  return  <h1>This is an h1 heading</h1>
-}
+
 
 function App() {
+  const handleClick = () => {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    let userInput = prompt("Type a number");
+    alert(`Computer number: ${randomNum}, Your gyess: ${userInput}`);
+  };
 
   return (
     <div>
-      
+      <h1>Number guessing game</h1>
+      <button onClick={handleClick}>Guess a number between 1 and 3</button>
     </div>
-    
+
   );
 }
 
