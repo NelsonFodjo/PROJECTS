64f252ea42dc44b3985d92f864e602ef396bf041
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-06-11 18:56:01 +0400

    Enhanced test coverage and testing capabilities
    Changes include: Modified App.css, Modified App.jsx
    
    Modified 36 lines (+32 additions, -4 deletions)

--- REACT/testing/src/App.jsx
+++ REACT/testing/src/App.jsx
@@ -8,11 +8,11 @@ export default function App(){
 
   return (
     <div className='app-container'>
-      <h3>React Counter Application</h3>
+      <h3 className='main'>React Counter Application</h3>
       <h1 className='counter-heading'>Current number: {num}</h1>
       <div className='button-container'>
-      <button onClick={() => setNum(num + 1)} className='counter-button'>Add 1</button>
-      <button onClick={() => setNum(num - 1)} className='counter-button'>Subtract 1</button>
+      <button onClick={() => setNum(num + 1)} className='counter-button'>+</button>
+      <button onClick={() => setNum(num - 1)} className='counter-button'>-</button>
       </div>
     </div>
   );
