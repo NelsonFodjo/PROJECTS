aacd7d7e95e7f95fc5c03f38c6a8ee523366239d
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-06-11 17:56:01 +0400

    Enhanced test coverage and testing capabilities
    Changes include: Modified App.jsx
    
    Modified 21 lines (+10 additions, -11 deletions)

--- REACT/testing/src/App.jsx
+++ REACT/testing/src/App.jsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react'
 
-function App() {
+ export default function App() {
+
   const [winner, setWinner] = useState("Mexico");
+  const [inputText, setInputText] = useState('hello')
 
   const handleClick = () => {
     setWinner((prevWinner) =>
@@ -9,21 +11,18 @@ function App() {
     );
   };
 
+  const handleChange = () =>{
+    setInputText(event.target.value);
+  };
+
   return (
     <div>
       <h3>Who do you think will win the world cup?</h3>
       <h1>{winner} will win!</h1>
       <button onClick={handleClick}>Change</button>
+      <h2>Now let's explore some typing</h2>
+      <input value={inputText} onChange={handleChange}/>
+      <h1>You typed: {inputText}</h1>
     </div>
   );
 }
-
-export default App
-
-function handleLight(){
-  setLightOn((currentState) =>
-    currentState === true ? false : true
-  );
-};
-
-<button onClick={handleLight}>Switch</button>
