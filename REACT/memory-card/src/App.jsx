2308b5f19d1e6182e2550109330bba7cd8548cef
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-06-14 19:44:39 +0400

    Introduced new files to expand functionality
    Changes include: Modified REACT/memory-card/
    
    Added 10 new lines

---
+++ REACT/memory-card/src/App.jsx
@@ -0,0 +1,12 @@
+import GameHeader from "./components/GameHeader"
+
+function App() {
+
+  return (
+    <>
+      <GameHeader />
+    </>
+  )
+}
+
+export default App
---
+++ REACT/memory-card/src/components/GameHeader.jsx
@@ -0,0 +1,15 @@
+export default function GameHeader(){
+    return(
+        <div className="game-header">
+            <h1>Memory Card Game</h1>
+            <div className="stats">
+                <div className="stat-item">
+                    <span className="stat-label"> Score:t</span></div>
+                <div className="stat-item">
+                    </div>            
+                </div>
+    </div>
+    );
+}
+    
+    
