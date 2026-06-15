6535e03aa4c3b89c3afdd245569ef816595597f3
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-06-15 21:09:32 +0400

    Enhanced 1 file with improvements
    Changes include: Modified App.jsx
    
    Modified 23 lines (+15 additions, -8 deletions)

--- REACT/memory-card/src/App.jsx
+++ REACT/memory-card/src/App.jsx
@@ -13,10 +13,14 @@ function App() {
   const [flippedCards, setFlippedCards] = useState([])
   const [score, setScore] = useState(0)
   const [isChecking, setIsChecking] = useState(false)
+  const [moves, setMoves] = useState(0)
+
+
 
   const initializeGame = () => {
     //shuffling to be done later
-
+    cardValues.sort(() => Math.random())
+st
     const finalCards =  cardValues.map((value, index) => ({
       id: index,
       value,
@@ -30,6 +34,7 @@ function App() {
   useEffect(() => { initializeGame() }, [])
 
   const handleCardClick = (card) => {
+    
   // do not allow clicking if the card is flipped or matched.
   if (card.isFlipped || card.isMatched || isChecking) {
     return;
@@ -46,31 +51,33 @@ function App() {
   setCards(newCards)
 
   const newFlippedCards = [...flippedCards, card.id]
-  console.log(newFlippedCards)
 
   setFlippedCards(newFlippedCards)
   //check for a match if two cards are fillped
   if(newFlippedCards.length === 2){
+    setMoves(moves + 1)
     const firstCard = cards.find(c => c.id === newFlippedCards[0])
     
     if (firstCard.value === card.value){
       setScore(score + 1)
       setFlippedCards([])
-      const newCards = cards.map((c) => {
-        if(c.id === card.id){
-          return{...c, isFlipped: true, isMatched: true}
+      const matchedCards = newCards.map((c) => {
+        if(newFlippedCards.includes(c.id)){
+          return{...c, isMatched: true}
         }
         else{
           return c
         }
       })
-      setCards(newCards)
+
+
+      setCards(matchedCards)
       
     } else {
       setIsChecking(true)
       setTimeout(() => {
         setCards(prev => prev.map(c =>
-          newFlippedCards.includes(c.id ? { ...c, isFlipped: false} : c)
+          newFlippedCards.includes(c.id) ? { ...c, isFlipped: false} : c
         ))
         setFlippedCards([])
         setIsChecking(false)
@@ -86,7 +93,7 @@ function App() {
 
   return (
     <>
-      <GameHeader score={score} moves={0}/>
+      <GameHeader score={score} moves={moves}/>
 
       <div className="cards-grid">
         {cards.map((card) => (
