8f4d51a4aa27fd3dea7c8b31defafb9d641a332f
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-07-06 18:00:08 +0400

    Enhanced 2 files with improvements
    Changes include: Modified App.jsx, Modified reducer.js
    
    Modified 20 lines (+18 additions, -2 deletions)

--- REACT/task-manager/src/App.jsx
+++ REACT/task-manager/src/App.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useReducer } from "react";
+
+function taskReducer(){
+
+}
 
 export default function App(){
+  consinitialState = {tasks: []}
+  const [state, dispatch] = useReducer(taskReducer, initialState);
+  
   return(
     <>
     <h1>Hi Nelson</h1>
--- REACT/task-manager/src/reducer.js
+++ REACT/task-manager/src/reducer.js
@@ -1,1 +1,10 @@
-functi
+export default function taskReducer(state, action){
+    switch(action.type){
+        case "ADD_TASK":
+            return { ...state,
+                . = [...state, action.payload]
+            }
+        default:
+            return state;
+    }
+}
