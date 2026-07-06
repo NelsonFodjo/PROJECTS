96c0bfb902ab083c1bd886df06682172f62af0d0
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-07-06 22:00:08 +0400

    Enhanced 3 files with improvements
    Changes include: Updated 2 React components, Modified reducer.js
    
    Modified 25 lines (+17 additions, -8 deletions)

--- REACT/task-manager/src/App.jsx
+++ REACT/task-manager/src/App.jsx
@@ -1,16 +1,18 @@
-import React, { useReducer } from "react";
-
-function taskReducer(){
-
-}
+import { useReducer } from "react";
+import taskReducer from "./reducer";
+import TaskForm from "./components/TaskForm";
+const initialState = {id: Date.now(), tasks: [], completed : false}
 
 export default function App(){
-  consinitialState = {tasks: []}
-  const [state, dispatch] = useReducer(taskReducer, initialState);
+  
+  const [state, dispatch] = useReducer(taskReducer, {initialState});
+  cons
+  
   
   return(
     <>
     <h1>Hi Nelson</h1>
+    <TaskForm state/>
     </>
   )
 }
---
+++ REACT/task-manager/src/components/TaskForm.jsx
@@ -0,0 +1,7 @@
+export default function TaskForm(p){
+    return(
+        <>
+        
+        </>
+    )
+}
