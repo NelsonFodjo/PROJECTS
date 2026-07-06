9743241e41467999e48179483158555f91b30e4e
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-07-06 23:30:08 +0400

    Enhanced 1 file with improvements
    Changes include: Modified TaskList.jsx
    
    Modified 4 lines (+3 additions, -1 deletions)

--- REACT/task-manager/src/components/TaskList.jsx
+++ REACT/task-manager/src/components/TaskList.jsx
@@ -3,7 +3,9 @@ import TaskItem from "./TaskItem";
 export default function TaskList({ tasks }){
     return (
         <ul>
-            {tasks.}
+            {tasks.map((task) => (
+                <TaskItem key={task.id} task={task} />
+            ))}
         </ul>
     )
 }
