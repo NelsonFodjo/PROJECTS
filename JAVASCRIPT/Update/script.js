f78a35d4ac82344ec843d6978d20ac44565e386a
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-07-22 12:24:23 +0400

    Updates 2 file(s) with structural changes.
    Modified 17 lines (+12 additions, -5 deletions)

--- JAVASCRIPT/Update/script.js
+++ JAVASCRIPT/Update/script.js
@@ -1,4 +1,10 @@
-function change(){
-    document.getElementById("first").textContent = "here is the updated text"
-}
+// function pair(arr){
+//     let newarr = arr.filter((x) => x % 2 == 0)
+//     return newarr.reduce((a, b) => a + b)
+// }
 
+// console.log(pair([1,2,3,4,5,6,7,8,9,10]))
+
+let user_input = document.getElementById("user_input").textContent
+
+user_input.addEventListener
